498285fa114fe60041d26b8a4d34e9215b768f19
dhydrated <dhydrated@gmail.com>
2015-12-09 23:25:56 +0800

    Add test

--- test/integration/default/serverspec/recipe_spec.rb
+++ test/integration/default/serverspec/recipe_spec.rb
@@ -1,6 +1,18 @@
 require "serverspec"
 set :backend, :exec
 
-describe package "xvfb" do
+describe package "curl" do
   it { should be_installed }
+end
+
+describe package "git" do
+  it { should be_installed }
+end
+
+describe file "/vagrant/home/projects/sample_app/Gemfile" do
+  it { should exist }
+end
+
+describe file "/vagrant/home/projects/sample_app/run_post_commands" do
+  it { should exist }
 end
