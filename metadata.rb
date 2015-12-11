63608bb39f8a7e365f0b8a7cc7fca15ed49960e0
dhydrated <dhydrated@gmail.com>
2015-12-11 09:48:18 +0800

    Add source_url

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures dev_env'
 long_description 'Installs/Configures dev_env'
 version          '0.1.0'
+source_url       "https://github.com/dhydrated/dev_env_cookbook"
 
 supports "ubuntu"
 supports "debian"
