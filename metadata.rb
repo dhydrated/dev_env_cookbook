3ce0ac1704081f548aad764c69988994f48cb956
dhydrated <dhydrated@gmail.com>
2015-12-11 09:28:41 +0800

    Add 'git' dependency

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,10 @@ license          'All rights reserved'
 description      'Installs/Configures dev_env'
 long_description 'Installs/Configures dev_env'
 version          '0.1.0'
+
+supports "ubuntu"
+supports "debian"
+
+depends "git"
+
+
